b02ac9c06da7e42adace686529fa75ea91e3cc88
abden555 <kweb.abden@gmail.com>
2015-06-08 13:27:11 +0530

    Update sqllite.js

--- www/js/sqllite.js
+++ www/js/sqllite.js
@@ -146,6 +146,7 @@ function ExportDBValues() {
 		var rescheck = '';
         for (var i = 0; i < result.rows.length; i++) {
 			var row = result.rows.item(i);
+			alert(row.promoterID);
 			$.ajax({
 				url: 'http://pixeledges.com/test.php',
 				type: "POST",
